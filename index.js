8937b6859eab561397c46b748322e1957223ab9c
Aitor <aitor.db@opendeusto.es>
2016-05-31 16:58:14 +0200

    pouchdb-server is working now thanks to 0.0.0.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var server = require("./server");
-var db_manager = require("./database")
-var db_domain = "localhost"; // database domain
+var db_manager = require("./database");
+var db_domain = "0.0.0.0"; // database domain
 var db_port =  "5984" // database port
 var server_port = "8888"; // Node.js server port
 db_manager.initialize(db_domain, db_port);
