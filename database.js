673288f38194b317e05c4788ec6fafad3bda0ca0
Aitor <aitor.db@opendeusto.es>
2016-09-16 11:25:45 +0200

    estaba comitteando en el lado cliente en vez del lado servidor

--- database.js
+++ database.js
@@ -112,12 +112,9 @@ function loadStaff() {
             console.log(temp[0]);
             console.log(temp[8]);
             console.log(typeof temp[8]);
-            console.log(temp[8] == 'true');
-            console.log(temp[8] === 'true');
-            console.log(temp[8] == true);
-            console.log(temp[8].toString() == 'true');
-            console.log(temp[8].toString() === 'true');
-            console.log(temp[8].toString() == true);
+            console.log(temp[8].replace(/^[truefalse]/g, "") == 'true');
+            console.log(temp[8].replace(/^[truefalse]/g, "") == 'true') === 'true');
+            console.log(temp[8].replace(/^[truefalse]/g, "") == 'true') == true);
             dt = temp[8];
         }
         _dbstaff.put({
