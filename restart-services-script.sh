9f560eff6dd9edaa240bfc40352037643c062eab
Aitor DB <aitor.db@opendeusto.es>
2017-07-10 14:09:00 +0200

    .gitignore added to avoid uploading to the repository files that should not be uploaded or are not necessary, such as: the database, internal folders related to the DB, error LOGs etc.

--- restart-services-script.sh
+++ restart-services-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "hola"
-echo "Starting restart script to relaunch the database and server services" > /home/aitor/starting.txt
 
-nohup sudo pouchdb-server -o 0.0.0.0 --port 5984 > /dev/null 2>"/home/aitor/UDBeaconizer_Server/nohup_errors_log.txt" &
+cd /home/aitor/UDBeaconizer_Server/
+nohup pouchdb-server -o 0.0.0.0 --port 5984 > /dev/null 2>"/home/aitor/UDBeaconizer_Server/nohup_errors_log.txt" &
+sleep 10 # we wait 10 seconds because the following command (2nd service to launch) depends in the previous one.  
 nohup node /home/aitor/UDBeaconizer_Server/index.js > /dev/null 2>"/home/aitor/UDBeaconizer_Server/nohup_errors_log.txt" &
 
-echo "Everything seems to be working fine. Are the processes or services launched yet?" > "/home/aitor/conclusion.txt"
+echo "(Current pwd: " $(pwd) ") Everything seems to be working fine. Are the processes or services launched yet?" > "/home/aitor/conclusion.txt"
