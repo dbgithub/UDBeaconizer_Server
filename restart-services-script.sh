bb5fe8423d5807d3943d07b8705ac9d806866d6f
Aitor DB <aitor.db@opendeusto.es>
2017-07-12 13:51:56 +0200

    commenting a line that is not necessary to put it in production time

--- restart-services-script.sh
+++ restart-services-script.sh
@@ -5,4 +5,4 @@ nohup pouchdb-server -o 0.0.0.0 --port 5984 > /dev/null 2>"/home/aitor/UDBeaconi
 sleep 10 # we wait 10 seconds because the following command (2nd service to launch) depends in the previous one.  
 nohup node /home/aitor/UDBeaconizer_Server/index.js > /dev/null 2>"/home/aitor/UDBeaconizer_Server/nohup_errors_log.txt" &
 
-echo "(Current pwd: " $(pwd) ") Everything seems to be working fine. Are the processes or services launched yet?" > "/home/aitor/conclusion.txt"
+#echo "(Current pwd: " $(pwd) ") Everything seems to be working fine. Are the processes or services launched yet?" > "/home/aitor/conclusion.txt"
